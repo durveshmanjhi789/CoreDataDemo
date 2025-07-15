1a6d8fb635e58779e8b2dad6eb5d8627e3388eaf
Durvesh Manjhi <durveshmanjhi@Mac.lan>
2025-07-15 11:12:31 +0530

    core data demo

---
+++ CoreDataProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataProject
+//
+//  Created by Durvesh Manjhi on 15/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
